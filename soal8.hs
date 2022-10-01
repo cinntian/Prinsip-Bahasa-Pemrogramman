598cc650cb64b28a7beaa525db015cf119e81c82
cinntian <95114138+cinntian@users.noreply.github.com>
2022-10-01 20:10:10 +0700

    Delete soal8.hs

--- soal8.hs
+++
@@ -1,8 +0,0 @@
-import Data.Array
-
-myArray = array (1, 3) [(1, "a"), (2, "b"), (3, "c")]
-
-showBounds = bounds myArray
-showIndices = indices myArray
-showElems = elems myArray
-showAssocs = assocs myArray
