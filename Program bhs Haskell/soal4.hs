12cf014890b52d6c6f5518af24a0dbf51a7e02f2
cinntian <95114138+cinntian@users.noreply.github.com>
2022-10-01 20:34:19 +0700

    revisi

--- Program bhs Haskell/soal4.hs
+++ Program bhs Haskell/soal4.hs
@@ -1,4 +1,4 @@
-is_prime :: Int -> Bool
-is_prime 1 = False
-is_prime 2 = True
-is_prime n | (length [x | x <- [2 .. n-1], mod n x == 0]) > 0 = False | otherwise = True
+cekPrime :: Int -> Bool
+cekPrime 1 = False
+cekPrime 2 = True
+cekPrime n | (length [x | x <- [2 .. n-1], mod n x == 0]) > 0 = False | otherwise = True
